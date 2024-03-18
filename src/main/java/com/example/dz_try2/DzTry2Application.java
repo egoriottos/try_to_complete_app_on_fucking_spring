39a5b4db724904c1e8abda0ba5ebd6cd2827e2e5
egoriottos <titkov1993@bk.ru>
2024-03-18 23:05:47 +0300

    first commit

---
+++ src/main/java/com/example/dz_try2/DzTry2Application.java
@@ -0,0 +1,13 @@
+package com.example.dz_try2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzTry2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzTry2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dz_try2/DzTry2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dz_try2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzTry2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
